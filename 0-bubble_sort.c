1abc73ec96c225f3847e26b60b2498bafb2a50fd
fabianatellechea <fabianagtellechea@gmail.com>
2023-09-08 15:07:50 -0700

    fixed: task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,13 +11,13 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = i + 1; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[i];
-				array[i] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
